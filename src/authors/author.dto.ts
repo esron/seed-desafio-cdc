dece49eaa28eb840f573929f4c2fcee12d9bee9b
Esron <esron.dtamar@gmail.com>
2021-11-29 20:47:46 -0300

    Add validations to authorDto

--- src/authors/author.dto.ts
+++ src/authors/author.dto.ts
@@ -1,5 +1,14 @@
+import { IsEmail, IsNotEmpty, Length } from 'class-validator';
+
 export class AuthorDTO {
+  @IsNotEmpty()
   name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsNotEmpty()
+  @Length(0, 400)
   description: string;
 }
