6de87be9a7efc4d55e8b6f06db1622debfff298a
Esron <esron.dtamar@gmail.com>
2021-11-29 20:36:20 -0300

    Configure mysql database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthorsController } from './authors/authors.controller';
+import { Author } from './authors/author.entity';
+import { AuthorsModule } from './authors/authors.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, AuthorsController],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'example',
+      database: 'test',
+      entities: [Author],
+      synchronize: true,
+    }),
+    AuthorsModule,
+  ],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
