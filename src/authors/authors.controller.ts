130d63199885b0b3bc38c78e53e96418b6dfdeaf
Esron <esron.dtamar@gmail.com>
2021-11-29 20:34:44 -0300

    Create a author controller

--- src/authors/authors.controller.ts
+++ src/authors/authors.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthorDTO } from './author.dto';
+import { Author } from './author.entity';
+import { AuthorsService } from './authors.service';
 
 @Controller('authors')
-export class AuthorsController {}
+export class AuthorsController {
+  constructor(private readonly authorsService: AuthorsService) {}
+
+  @Post()
+  create(@Body() author: AuthorDTO): Promise<Author> {
+    return this.authorsService.create(author);
+  }
+}
