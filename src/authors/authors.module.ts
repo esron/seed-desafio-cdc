39b7f8edda495d1df0661fc70f625e6371bb46be
Esron <esron.dtamar@gmail.com>
2021-11-30 19:43:43 -0300

    Refactora pra simplificar

--- src/authors/authors.module.ts
+++ src/authors/authors.module.ts
@@ -2,11 +2,9 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Author } from './author.entity';
 import { AuthorsController } from './authors.controller';
-import { AuthorsService } from './authors.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Author])],
-  providers: [AuthorsService],
   controllers: [AuthorsController],
 })
 export class AuthorsModule {}
