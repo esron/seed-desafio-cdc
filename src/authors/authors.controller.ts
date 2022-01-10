61852507439cd07f6b4565864fda5523c7d9ce98
Esron <esron.dtamar@gmail.com>
2022-01-10 20:26:43 -0300

    Remove authors service

--- src/authors/authors.controller.ts
+++ src/authors/authors.controller.ts
@@ -1,14 +1,18 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { AuthorDTO } from './author.dto';
 import { Author } from './author.entity';
-import { AuthorsService } from './authors.service';
 
 @Controller('authors')
 export class AuthorsController {
-  constructor(private readonly authorsService: AuthorsService) {}
+  constructor(
+    @InjectRepository(Author)
+    private readonly authorsRepository: Repository<Author>,
+  ) {}
 
   @Post()
-  create(@Body() author: AuthorDTO): Promise<Author> {
-    return this.authorsService.create(author);
+  public async create(@Body() author: AuthorDTO): Promise<Author> {
+    return this.authorsRepository.save(author);
   }
 }
